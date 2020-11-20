0b177d1f8750dc070e136f1b4d536ce4d802c841
YuliiaOsadchukkk <72582368+YuliiaOsadchukkk@users.noreply.github.com>
2020-11-20 19:35:05 +0200

    lab_5.4withUnitTest

---
+++ UnitTest4/UnitTest4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.4/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest4
+{
+	TEST_CLASS(UnitTest4)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = S1(2, 3, 0);
+				Assert::AreEqual(t, 0);
+		}
+	};
+}
